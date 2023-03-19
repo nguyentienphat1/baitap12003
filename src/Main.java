0e6b928194496693912e14fcb7cb495ca7d927bf
Khac Huy <nguyentienphat13@gmail.com>
2023-03-19 19:26:18 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int a,b,c;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("nhap a");
+        a= scanner.nextInt();
+        System.out.println(" nhap b");
+        b= scanner.nextInt();
+        System.out.println("nhap c");
+        c= scanner.nextInt();
+        if (a>b){
+            if (a>c){
+                System.out.println("a la so lon nhat");
+            }else {
+                System.out.println("c la so lon nhat");
+            }
+        }else {
+            if (b>c){
+                System.out.println("b la so lon nhat");
+            }else {
+                System.out.println("c la so lon nhat");
+            }
+        }
+    }
+}
